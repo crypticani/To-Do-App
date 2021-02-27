7608f61eec7590ef373deb97ce327aa0d171a4ca
crypticani <aniket.kumar@hotmail.com>
2021-02-27 13:49:14 +0530

    created priority and category based list view

--- todoapp/admin.py
+++ todoapp/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from todoapp import models
+from todoapp.models import todoModel
 
 
-admin.site.register(models.todoModel)
+class myToDo(admin.ModelAdmin):
+    list_display = ['heading', 'priority', 'category', 'assigned_date', 'due_date', 'done']
+    list_filter = ['priority', 'category', 'done']
+
+admin.site.register(todoModel, myToDo)
